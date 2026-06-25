7f6f86c1e22a0496b060af36b1ce55dea8e4f6e6
Ventsislava Petrova <ventsislavapetrova@192.168.100.2>
2026-06-25 19:01:44 +0300

    Created two more DTOs, switched boolean to Boolean for just the two of them to have the null option.

---
+++ src/main/java/com/featureflags/dto/UpdateFeatureFlagDto.java
@@ -0,0 +1,29 @@
+package com.featureflags.dto;
+
+// For the PATCH /.../flags/{id}.
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateFeatureFlagDto {
+
+    @NotBlank(message = "Flag name must not be blank.")
+    @Size(min = 2, max = 50, message = "Flag name must be btw 2 and 50 chars.")
+    @Pattern(regexp = "^[a-z0-9]+(-[a-z0-9]+)*$", message = "Flag name must be kebab-case.")
+    private String name;
+
+    @Size(max = 150, message = "Description must not be more than 150 chars.")
+    private String description;
+
+    @Builder.Default
+    private Boolean enabled = false;
+}
